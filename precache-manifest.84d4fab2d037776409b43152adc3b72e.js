927af525fda8f8551e4e7c8989e1484cbc686e98
JonReactJS <jon_github@outlook.com>
2019-01-09 13:15:01 -0600

    Updates

---
+++ precache-manifest.84d4fab2d037776409b43152adc3b72e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e54cdcc9e27a6a6ed13",
+    "url": "/climarestapi/static/js/runtime~main.7e54cdcc.js"
+  },
+  {
+    "revision": "321053eae584a3cbd213",
+    "url": "/climarestapi/static/js/main.321053ea.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/climarestapi/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "321053eae584a3cbd213",
+    "url": "/climarestapi/static/css/main.e8630c50.chunk.css"
+  },
+  {
+    "revision": "92bdeb9f0ca6ee2cf685e82e18058e59",
+    "url": "/climarestapi/index.html"
+  }
+];
